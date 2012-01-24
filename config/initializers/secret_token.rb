4ca6f066d9a8e4b2e145938cd0f33e23dfab2908
Matthew Rudy Jacobs <MatthewRudyJacobs@gmail.com>
2012-01-24 12:58:33 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OneSkyRailsExample::Application.config.secret_token = '78646ee309174676982ac9583a6c59c028d3c019a4c6f54c7668b817e98a1b57ad9b4dbd3c14181ac408934e3d6de825bfc7392c4106c64a103d33a8ac779d5b'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+OneSkyRailsExample::Application.routes.draw do
+  root :to => "home#index"
+end
